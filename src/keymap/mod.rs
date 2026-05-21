b762cefcfad0e5cc3a3e5311dc4dcded47c0f4dc
Ryan Meline <ryan.c.meline@gmail.com>
2026-05-21 14:27:50 -0700

    Added to do notes

---
+++ src/keymap/mod.rs
@@ -0,0 +1,4 @@
+//do something like
+// let action = keymap::handle_event(read()?);
+//in main
+//where action is an Action enum and is then run through a match statement in main to call editor functions
--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -10,9 +10,13 @@ use crossterm::{
 
 pub struct TerminalGuard;
 
+//destructor (called when main ends or panic happens)
 impl Drop for TerminalGuard {
     fn drop(&mut self) {
         let _ = execute!(stdout(), LeaveAlternateScreen);
         let _ = disable_raw_mode();
     }
 }
+
+
+// Add the render here, call it at the beginning of each loop
