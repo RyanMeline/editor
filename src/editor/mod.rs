4eaca490b0a3fef9a03939b2317a5343b828bac8
Ryan Meline <ryan.c.meline@gmail.com>
2026-05-30 23:49:09 -0700

    Fixed deleting lines and making new lines

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -14,7 +14,7 @@ use crossterm::{
     style::Print,
     terminal::{
         Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode,
-        enable_raw_mode,
+        enable_raw_mode, size,
     },
 };
 
@@ -52,6 +52,26 @@ impl Editor {
         }
     }
 
+    /// Updates position tracker on the bottom left of the terminal
+    pub fn update_position_tracker(&self) {
+        let (cols, rows) = crossterm::terminal::size().unwrap();
+        let target_col = cols.saturating_sub(1);
+        let target_row = rows.saturating_sub(1);
+        execute!(stdout(), Hide).unwrap();
+        execute!(stdout(), MoveTo(target_col, target_row)).unwrap();
+        execute!(stdout(), Print("^".to_string())).unwrap();
+        execute!(
+            stdout(),
+            MoveTo(
+                self.cursor_column.try_into().unwrap(),
+                self.cursor_line_no.try_into().unwrap()
+            )
+        )
+        .unwrap();
+        execute!(stdout(), Show).unwrap();
+    }
+
+    /// Redraws current line
     fn redraw_line(&self) {
         //Hide cursor and move to the start of the line
         execute!(stdout(), Hide).unwrap();
@@ -66,6 +86,7 @@ impl Editor {
         execute!(stdout(), Show).unwrap();
     }
 
+    /// Redraws entire screen
     fn redraw_screen(&self) {
         //draw text inside screen bounds
         execute!(stdout(), Hide).unwrap();
@@ -100,6 +121,15 @@ impl Editor {
         //redraw screen
     }
 
+    /// Inserts character at current cursor location
+    ///
+    /// # Arguments
+    /// * `c` - Character to be entered
+    ///
+    /// # Examples
+    /// ```
+    /// editor.insert_char("a");
+    /// ```
     pub fn insert_char(&mut self, c: char) {
         //execute!(stdout(), Print(c.to_string())).unwrap();
         self.lines[self.cursor_line_no].insert_char(c, self.cursor_column.try_into().unwrap());
@@ -107,16 +137,54 @@ impl Editor {
         self.redraw_line();
     }
 
+    /// Enters a new line
+    /// Moves text after cursor to the new line
     pub fn new_line(&mut self) {
         //check if gap_end is == buf.len()-1, if its *not*, take all the text after it, and push
         //that to a new line
-        let l_new = Line::new();
-        self.lines.insert(self.cursor_line_no + 1, l_new);
-        self.cursor_line_no += 1;
-        self.cursor_column = 0;
-        self.redraw_screen();
+        //If the buffer's end index is at the end of the line (enter is pressed at the end of the
+        //line)
+        if self.lines[self.cursor_line_no as usize].gap_end
+            == (self.lines[self.cursor_line_no as usize].buf.len() - 1)
+                .try_into()
+                .unwrap()
+        {
+            let l_new = Line::new();
+            self.lines.insert(self.cursor_line_no + 1, l_new);
+            self.cursor_line_no += 1;
+            self.cursor_column = 0;
+            self.redraw_screen();
+        } else {
+            // take all the text from gap_end to line_end, copy it, delete it (move buffer end to end
+            //of line), make a new line, add that text to the new line
+            //get text between gap_end and line_end
+            let temp_str: String = self.lines[self.cursor_line_no].get_text_after_buffer();
+            //move buffer end to line_end
+            self.lines[self.cursor_line_no].gap_end = (self.lines[self.cursor_line_no].buf.len()
+                - 1)
+            .try_into()
+            .unwrap();
+            self.lines[self.cursor_line_no].text_length -= temp_str.chars().count() as u16;
+            //make a new line
+            let l_new = Line::new();
+            self.lines.insert(self.cursor_line_no + 1, l_new);
+            self.cursor_line_no += 1;
+            self.cursor_column = 0;
+            //insert previously coppied text into new line
+            for c in temp_str.chars() {
+                self.lines[self.cursor_line_no]
+                    .insert_char(c, self.cursor_column.try_into().unwrap());
+                self.cursor_column += 1;
+            }
+            execute!(stdout(), Print(&temp_str)).unwrap();
+            self.lines[self.cursor_line_no].move_buffer(0);
+            self.cursor_column = 0;
+            self.redraw_screen();
+        }
     }
 
+    /// Deletes character infront of cursor
+    /// Moves gap buffer start point ontop of character before buffer, redraws line
     pub fn delete_char(&mut self) {
         if self.cursor_column == 0 {
             if self.cursor_line_no == 0 {
@@ -140,6 +208,8 @@ impl Editor {
         self.redraw_line()
     }
 
+    /// Deletes character after cursor position
+    /// Moves gap buffer end point over character, redraws line
     pub fn delete_char_forward(&mut self) {
         if self.lines[self.cursor_line_no].gap_end as usize
             == self.lines[self.cursor_line_no].buf.len() - 1
@@ -156,18 +226,25 @@ impl Editor {
         self.redraw_line()
     }
 
+    /// Moves cursor in entered direction
+    ///
+    /// # Arguments
+    /// * `dir`: `Direction` - Direction of movement
+    ///
+    /// ```
+    /// enum Direction
+    ///     Direction::Left
+    ///     Direction::Right
+    ///     Direction::Up
+    ///     Direction::Down
+    /// ```
     pub fn move_cursor(&mut self, dir: Direction) {
-        //Need to do checks when going up and down to check line length
-        //to stop jumping down to somewhere the line doesnt exist
-
         let c_line_no: u16 = self.cursor_line_no.try_into().unwrap();
         let c_column: u16 = self.cursor_column.try_into().unwrap();
 
         match dir {
             Direction::Left => {
-                if self.cursor_column == 0 {
-                    //do nothing
-                } else {
+                if self.cursor_column != 0 {
                     self.cursor_column -= 1;
                     execute!(stdout(), MoveTo(c_column - 1, c_line_no)).unwrap();
                     self.lines[self.cursor_line_no]
@@ -176,8 +253,7 @@ impl Editor {
                 }
             }
             Direction::Right => {
-                if self.cursor_column == self.lines[self.cursor_line_no].text_length as usize {
-                } else {
+                if self.cursor_column != self.lines[self.cursor_line_no].text_length as usize {
                     self.cursor_column += 1;
                     execute!(stdout(), MoveTo(c_column + 1, c_line_no)).unwrap();
                     self.lines[self.cursor_line_no]
@@ -186,10 +262,7 @@ impl Editor {
                 }
             }
             Direction::Up => {
-                if self.cursor_line_no == 0 {
-                    //at top
-                    //do nothing
-                } else {
+                if self.cursor_line_no != 0 {
                     if self.lines[self.cursor_line_no - 1].text_length < c_column {
                         self.cursor_column = self.lines[self.cursor_line_no - 1]
                             .text_length
@@ -208,10 +281,7 @@ impl Editor {
                 }
             }
             Direction::Down => {
-                if self.cursor_line_no == self.lines.len() - 1 {
-                    //at bottom
-                    //do nothing
-                } else {
+                if self.cursor_line_no != self.lines.len() - 1 {
                     if self.lines[self.cursor_line_no + 1].text_length < c_column {
                         self.cursor_column = self.lines[self.cursor_line_no + 1]
                             .text_length
@@ -234,6 +304,8 @@ impl Editor {
 
     fn save(&mut self) {}
 
+    /// Deletes current line
+    /// Moves text to line above
     fn delete_line_backward(&mut self) {
         let temp = self.lines[self.cursor_line_no].get_line();
         self.lines.remove(self.cursor_line_no);
@@ -247,6 +319,8 @@ impl Editor {
         self.redraw_screen();
     }
 
+    /// Deletes line below current
+    /// Moves next line text to current line
     fn delete_line_forward(&mut self) {
         let temp = self.lines[self.cursor_line_no + 1].get_line();
         let temp_column = self.cursor_column;
@@ -270,6 +344,11 @@ impl Line {
         }
     }
 
+    /// Inserts char into line at given position
+    ///
+    /// # Arguments
+    /// * `c` - Character to be entered
+    /// * `current_index` - Cursor position within the line
     fn insert_char(&mut self, c: char, current_index: u16) {
         if self.gap_start == self.gap_end {
             //do something about if gap is full
@@ -281,6 +360,17 @@ impl Line {
         self.text_length += 1;
     }
 
+    /// Moves buffer start to current position, shifting displaced text to the other side of the buffer
+    ///
+    /// # Arguments
+    /// * `new_start` - Index in the line the gap buffer start is moving to
+    ///
+    /// # Examples
+    /// ```
+    /// ['C','u','r','r','e','n','t',_,_,_,_,'g','a','p']
+    /// Line::move_buffer(3);
+    /// ['C','u','r',_,_,_,_,'r','e','n','t','g','a','p']
+    /// ```
     fn move_buffer(&mut self, new_start: u16) {
         //where the char is input is to the left of where the gap is
         let init_gap_start = self.gap_start;
@@ -320,14 +410,31 @@ impl Line {
         //need to move all text that was in buffer to the left or right depending on direction
     }
 
-    //prints either side of buffer
+    /// Returns a String containing text from both sides of the gap buffer
+    ///
+    /// # Returns
+    /// * `String`
     fn get_line(&self) -> String {
         //                               no + 1 since its not ..=
-        let mut s1: String = self.buf[..self.gap_start as usize].iter().collect();
-        let s2: String = self.buf[(self.gap_end + 1) as usize..].iter().collect();
+        let mut s1: String = self.buf[..self.gap_start as usize]
+            .iter()
+            .filter(|&&c| c != '\0')
+            .collect();
+        let s2: String = self.buf[(self.gap_end + 1) as usize..]
+            .iter()
+            .filter(|&&c| c != '\0')
+            .collect();
 
         s1.push_str(&s2);
         s1
+    }
+
+    fn get_text_after_buffer(&self) -> String {
+        let mut temp_str: String = String::new();
+        for i in (self.gap_end as usize) + 1..self.buf.len() {
+            temp_str.push_str(&self.buf[i].to_string());
+        }
+        temp_str
     }
 }
 
